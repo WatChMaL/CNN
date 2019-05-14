1a8e72eafae20a17028d5d9b262aa608181b96cd
Abhishek Abhishek <akajal@beluga3.int.ets1.calculquebec.ca>
2019-05-14 19:34:36 -0400

    Merging debug and master

--- USER/main.py
+++ USER/main.py
@@ -146,6 +146,6 @@ if __name__ == '__main__':
     saveConfig(config)
     model = resnet.resnet18(num_input_channels=38, num_classes=3)
     nnet = net.Engine(model, config)
-    #nnet.train(epochs=5.0, save_interval=1000)
-    nnet.restore_state("state1000")
+    nnet.restore_state("state2400")
+    nnet.train(epochs=10.0, save_interval=1000)
     nnet.validate()
