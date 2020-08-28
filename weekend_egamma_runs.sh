d5d3cc9b34f82dd98349a831b629d4d9e0d03d64
calum-macdonald33 <calum.j.macdonald@hotmail.com>
2020-08-27 17:06:37 -0700

    update captions

---
+++ weekend_egamma_runs.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+python3 watchmal_cl.py --load long_egamma.ini
+python3 watchmal_cl.py --load long_egamma_transforms.ini
---
+++ weekend_runs.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+python3 watchmal_cl.py --load long_3class.ini
+python3 watchmal_cl.py --load long_3class_transforms.ini
